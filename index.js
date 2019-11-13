987d30bfd2946d43c195c69ca623e124ea79a514
Auree Aubert <contact@auree.me>
2019-11-13 22:11:39 +0100

    Introduce paginate aggregation

--- index.js
+++ index.js
@@ -1,96 +1,79 @@
-'use strict';
+async function getQuery(query, options) {
+  const docsQuery = this.find(query)
+    .select(options.select)
+    .sort(options.sort)
+    .skip(options.skip)
+    .limit(options.limit);
 
-/**
- * @package mongoose-paginate
- * @param {Object} [query={}]
- * @param {Object} [options={}]
- * @param {Object|String} [options.select]
- * @param {Object|String} [options.sort]
- * @param {Array|Object|String} [options.populate]
- * @param {Boolean} [options.lean=false]
- * @param {Boolean} [options.leanWithId=true]
- * @param {Number} [options.offset=0] - Use offset or page to set skip position
- * @param {Number} [options.page=1]
- * @param {Number} [options.limit=10]
- * @param {Number} [options.noPaging=false]
- * @param {Function} [callback]
- * @returns {Promise}
- */
-
-async function paginate(query, options, callback) {
-  query = query || {};
-  options = Object.assign({}, paginate.options, options);
-  let select = options.select;
-  let sort = options.sort;
-  let populate = options.populate;
-  let lean = options.lean || false;
-  let leanWithId = options.leanWithId ? options.leanWithId : true;
-  let limit = options.limit ? options.limit : 10;
-  let noPaging = options.noPaging || false 
-  let page, offset, skip, data;
-
-  if (options.offset) {
-    offset = options.offset;
-    skip = offset;
-  } else if (options.page) {
-    page = options.page;
-    skip = (page - 1) * limit;
-  } else {
-    page = 1;
-    offset = 0;
-    skip = offset;
-  }
-
-  let docsQuery = this.find(query)
-    .select(select)
-    .sort(sort)
-    .skip(skip)
-    .limit(limit)
-    .lean(lean);
-
-  if (populate) {
-    [].concat(populate).forEach((item) => {
+  if (options.populate) {
+    [].concat(options.populate).forEach(item => {
       docsQuery.populate(item);
     });
   }
-  data = {
-    docs: await docsQuery.exec(),
+
+  return docsQuery;
+}
+
+async function getAggregateQuery(query, options) {
+  return this.aggregate([
+    ...query,
+    { $sort: options.sort },
+    { $skip: options.skip },
+    { $limit: options.limit }
+  ]);
+}
+
+async function getCount(query) {
+  return this.count(query);
+}
+
+async function getAggregateCount(query) {
+  const countResponse = await this.aggregate([...query, { $count: "count" }]);
+
+  return countResponse[0].count;
+}
+
+async function paginate(query = {}, options = {}, isAggregation = false) {
+  if (!options.sort) {
+    options.sort = { _id: 1 };
+  }
+
+  if (!options.limit) {
+    options.limit = 10;
+  }
+
+  if (options.page) {
+    options.skip = (options.page - 1) * options.limit;
+  } else {
+    options.page = 1;
+    options.skip = 0;
+  }
+
+  const data = {
+    docs: !isAggregation
+      ? await getQuery.call(this, query, options)
+      : await getAggregateQuery.call(this, query, options)
   };
 
-  if (noPaging === false) {
-    data.count = await this.count(query).exec();
+  if (options.noPaging) {
+    data.count = !isAggregation
+      ? await getCount.call(this, query)
+      : await getAggregateCount.call(this, query);
   }
 
-  if (lean && leanWithId) {
-    data.docs.forEach((doc) => {
-        doc.id = String(doc._id);
-    });
-  }
-
-  let result = {
+  const result = {
     docs: data.docs,
-    limit: limit
+    limit: options.limit,
+    page: options.page
   };
 
-  if (offset !== undefined) {
-    result.offset = offset;
-  }
-
-  if (page !== undefined) {
-    result.page = page;
-
-    if (noPaging === false) {
-      result.total = data.count,
-      result.pages = Math.ceil(data.count / limit) || 1;
-    }
+  if (options.noPaging === false) {
+    result.total = data.count;
+    result.pages = Math.ceil(data.count / options.limit) || 1;
   }
 
   return result;
 }
-
-/**
- * @param {Schema} schema
- */
 
 module.exports = function(schema) {
   schema.statics.paginate = paginate;
